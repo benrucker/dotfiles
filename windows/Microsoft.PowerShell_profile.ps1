410af7707d8b7e28c0ad7e087ea337eabad6d27c
Ben Rucker <brucker117@gmail.com>
2026-01-20 22:27:15 -0800

    Add prompt

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,15 @@
 Set-Alias -Name g -Value git
+
+function prompt 
+{
+	$time = (Get-Date).ToShortTimeString() -replace " ", ""
+  
+  $currentPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path -replace [regex]::Escape($HOME + "\"), "~"
+
+
+  $esc = [char]27
+
+
+	$prompt = "$esc[32m$time$esc[38;5;14m $currentPath$esc[39m > "
+	$prompt
+}
